27751b1edff9bd0cb296a4b9e69ca93c449cf5b5
RyukieSama <ryukie.sama@gmail.com>
2019-11-16 16:27:00 +0800

    ADD: <Rank>

---
+++ RYLeiAPI/RankVulture.swift
@@ -0,0 +1,16 @@
+//
+//  MineVulture.swift
+//  RYLeiAPI
+//
+//  Created by 王荣庆 on 2019/11/16.
+//  Copyright © 2019 RyukieSama. All rights reserved.
+//
+
+import Foundation
+import Vulture
+
+extension VultureNativeAPI {
+    public enum RankVulture {
+        case showRank
+    }
+}
